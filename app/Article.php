ffa7407c9f3e5ec25518a2fd7e117ec11fb319b2
lmmacias1986 <lmmacias1986@gmail.com>
2016-08-01 16:33:50 -0500

    Update Article.php

--- app/Article.php
+++ app/Article.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 
-class Article extends Model implements SluggableInterface
+class Article extends Model 
 {
 
     protected $table = "Articles";
